be496450d4b44028bdeec4b65543ef40fee0d98a
adriano360ws-droid <adriano360ws@gmail.com>
2026-01-03 15:28:30 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,42 @@
-const CACHE_NAME = "moto-taxi-v1";
+const CACHE_NAME = "moto-taxi-v2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icone-192.png",
-  "./icone-512.png"
+  "/mototaxi-corrida/",
+  "/mototaxi-corrida/index.html",
+  "/mototaxi-corrida/manifest.json",
+  "/mototaxi-corrida/icone-192.png",
+  "/mototaxi-corrida/icone-512.png"
 ];
 
+/* ===== INSTALL ===== */
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+/* ===== ACTIVATE ===== */
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       )
     )
   );
   self.clients.claim();
 });
 
+/* ===== FETCH ===== */
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
