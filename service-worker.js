758a7b27d0d93d5bb7550289f4be91407f362115
adriano360ws-droid <adriano360ws@gmail.com>
2026-01-03 12:08:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
