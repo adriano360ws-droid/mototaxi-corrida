859f8a6b81019e0b1cce01ff30d56c7112834730
adriano360ws-droid <adriano360ws@gmail.com>
2026-01-03 10:38:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,35 @@
-const CACHE_NAME = "moto-taxi-adriano-v4";
+const CACHE_NAME = "moto-taxi-v1";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./icone-192.png",
+  "./icone-512.png"
 ];
 
-// Instala o Service Worker e salva arquivos no cache
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Ativa e remove caches antigos
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
       )
     )
   );
   self.clients.claim();
 });
 
-// Busca arquivos: cache primeiro, depois internet
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
